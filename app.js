f8ffb1582d7fba240be279eac34f57e1462929f6
Glitch (jje-hackathon) <none>
2020-11-07 22:00:15 +0000

    template pages and basic login

--- app.js
+++ app.js
@@ -7,22 +7,6 @@ const server = require('http').createServer(app);
 const port = 8080;
 const io = require('socket.io')(server);
 
-
-const cp = require('child_process')
-const util = require('util')
-const execFile = util.promisify(cp.execFile);
-
-const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-const Authenticator = require('ibm-watson/auth').IamAuthenticator;
-
-const fs = require('fs');
-
-const languageTranslator = new LanguageTranslatorV3({
-  version: '2018-05-01'
-});
-
-
-
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -37,6 +21,56 @@ server.listen(port, function() {
   console.log("Your app is listening on port " + port);
 });
 
+//STATUS ENUMERATION
+const status = {
+	NEGATIVE: 'negative',
+	POSITIVE: 'positive',
+	POTENTIAL: 'potential',
+	LOW_RISK: 'lowrisk',
+	SAFE : 'safe'
+}
+
+const cp = require('child_process')
+const util = require('util')
+const execFile = util.promisify(cp.execFile);
+
+const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+const Authenticator = require('ibm-watson/auth').IamAuthenticator;
+
+const fs = require('fs');
+
+const languageTranslator = new LanguageTranslatorV3({
+  version: '2018-05-01'
+});
+
+const nodemailer = require("nodemailer");
+const email_transporter = nodemailer.createTransport({
+	host: "smtp.gmail.com",
+	port: 587,
+	secure: false,
+	auth: {
+		user: "sixftapp20",
+		pass: "barackobama44@"
+	},
+});
+
+
+let user_data = {}
+
+readJson("user_data.json", function(error, data){
+  if(error){
+    console.log("Error parsing JSON file on startup (Error 001)")
+    console.log(error)
+    return;
+  }
+  
+  user_data = data;
+  console.log("Parsed data:")
+  console.log(user_data);
+  
+});
+
+
 
 
 function readJson(filePath, callback) {
@@ -45,9 +79,7 @@ function readJson(filePath, callback) {
             return callback && callback(err)
         }
         try {
-            console.time()
             var object = JSON.parse(fileData)
-            console.timeEnd();
             return callback && callback(null, object)
         } catch(err) {
             return callback && callback(err)
@@ -57,15 +89,7 @@ function readJson(filePath, callback) {
 
 
 
-readJson('test.json', function(error, data){
-  if(error){ 
-    console.log(error)
-    return;
-  }
-  //console.log(data);
-  console.log('done');
-  
-})
+
 
 
 
@@ -93,13 +117,101 @@ function translate(text, source, target, callback){
   })
 }
 
-
+/*
 translate("Hello, how are you?", "en", "es", function(translations) {
   console.log(translations);
 })
+*/
 
+//BIG RED BUTTON
+function positive(user_email, notification_message = "")
+{
+	let first_exposure = new Set();
+	let second_exposure = new Set();
+	user_data[user_email].status = status.POSITIVE;
 
+	//Adds each interation to the set of emails
+	user_data[user_email].interactions.forEach(interaction => {
+			first_exposure.add(interaction.email);
+	});
 
+	//Iterates through each email in first_exposure
+	first_exposure.forEach(email => {
+
+		userNotify(user_email, email, notification_message);
+		if(!first_exposure.has(email)){
+			second_exposure.add(email)
+		}
+		if(user_data[email].status != status.POSITIVE){
+		user_data[email] = status.POTENTIAL;
+		}
+	});
+
+	//Iterates through all the emails in second_exposures
+	second_exposure.forEach(email => {
+		userNotify(user_email, email, notification_message);
+		if(user_data[email].status != status.POSITIVE && user_data[email].status != status.POTENTIAL) {
+		user_data[email] = status.LOW_RISK;
+	}
+	});
+}
+
+async function userNotify(positive_email, recipiet_email, notification_message){
+  //TODO : Add the notification message and translate
+	if(positive_email === recipiet_email)
+	{
+		return;
+	}
+
+	let email_body = "";
+
+	if(user_data[recipiet_email].status === status.POTENTIAL)
+	{
+	let interaction_time = new Date();
+
+	user_data[recipiet_email].interactions.forEach(interaction => {
+			if(interaction.email === positive_email)
+			{
+				interaction_time = interaction.date;
+			}
+	});
+
+	email_body = `Dear ${user_data[recipiet_email].name},\n\n\tA recent interaction on Six Feet has tested positive for COVID-19.\nWe advise recieving a test and begining a self quarentine immediately.\n\nYou last interaction with this user was on : ${interaction_time.toDateString()}.\n\nStay safe,\nSix Feet Team`;
+
+}
+
+  
+else
+{
+	email_body = `Dear ${user_data[recipiet_email].name}\n\n\tA recent interaction on Six Feet has come in contact with a person who has tested positive for COVID-19\nWe advise recieving a test and adhearing to COVID-19 guidelines.\n\nStay safe,\nSix Feet Team`
+}
+  
+  if(notification_message != "")
+{
+	let initial_lang = user_data[positive_email].language;
+	let final_lang = user_data[positive_email].language;
+	if(initial_lang != final_lang)
+	{
+		translate(notification_message, initial_lang, final_lang, function(translations) {
+  notification_message = translations;
+})
+	}
+	email_body += "\n\nMESSAGE FROM USER : " + notification_message;
+}
+  
+	let info = await email_transporter.sendMail({
+		from: '"SIXFEET NOTIFICATION" <sixftapp20@gmail.com>',
+		to: recipiet_email,
+		subject: "A recent interaction has tested positive for COVID-19",
+		text: email_body
+	},function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+}
 
 
 
@@ -118,13 +230,37 @@ io.on('connection',(socket) => {
   console.log('A user has connected!');
   
   
-  socket.on('run cpp', (data) => {
+  socket.on('new-user', (data) => {
+    //data
+    /*
+     {
+      "email": email,
+      "data" : {
+        name: display_name,
+        password: password,
+        friends: [],
+        interactions: [],
+        language: [],
+        status: 0
+      }
+     }
+    */
+    user_data[data.email] = data.data;
+    if(user_data[data.email].name = 'PISSBABY')
+{
+	userNotify("wap@gmail.com", "urookim@gmail.com", "PISS");
+}
+    //write user_data to json file
+    const user_json = JSON.stringify(user_data, null, 2);
+    fs.writeFile('user_data.json', user_json, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+
+    });
     
-    getCPPOutput(data.filename, data.args, (output) => {
-      socket.emit('cpp output', {filename: data.filename, output: output})
-    })
-    
-  })
+  });
   
   
   socket.on('disconnect', () => {
--- public/client.js
+++ public/client.js
@@ -1,19 +1,152 @@
 /*global io*/
 
+/* redirects to this website link
+window.onload = function() {
+  window.location.href = "https://www.wikipedia.org/";
+}
+*/
+
 //initialize client socket
 let socket = io();
 
+$(document).ready(function(){
+  makeScreenVisible('welcome-screen');
+});
 
-//add an event listener to the test button
-document.getElementById("testButton").addEventListener('click', function(){
-  console.log('clicked button');
-  socket.emit('run cpp', { filename: 'cpp_js.exe', args: ['data.txt']})
+
+function makeScreenVisible(screenId){
+  $('.screen').css('display', 'none');
+  $('#'+screenId).css('display', 'flex');
+}
+
+
+function makePageVisible(pageId){
+  $('.page').css('display', 'none');
+  $('#'+pageId).css('display', 'flex');
+}
+
+// takes input from signup screen and checks if all fields are valid, if so sends data to be stored and brings to main screen
+function confirmSignup(email, display_name, password, confirm){
+  return true;
+  console.log('confirming signup');
+  if (!confirmPassword(password)){
+    console.log('invalid password')
+    return false;
+  }
+  if (!confirmEmail(email)){
+    console.log('invalid email')
+    return false;
+  }
+  if (!confirmDisplay(display_name)){
+    console.log('invalid display')
+    return false;
+  }
+  if(password !== confirm){
+    console.log('passwords don\'t match')
+    return false;
+  }
+  
+  
+  console.log('returning true')
+  return true;
+}
+
+function confirmDisplay(display_name){
+  if (display_name && isNaN(display_name))
+      return true
+  return false
+}
+
+function confirmPassword(password){
+  //At least 6 characters
+  //First character must be a letter
+  //Other characters can be alphanumeric + underscore
+  const valid = password.match(/^[A-Za-z]\w{5,}$/);
+  if(!valid) 
+    // ################
+    // show password error
+    return false;
+  return true
+}
+
+function confirmEmail(email){
+  //check if unique
+  if(!email.match(/^\w{1,}@\w{1,}.\w{1,}$/))
+    return false;
+  return true;
+}
+
+
+
+
+
+//add event listener to login button
+document.getElementById('login-button').addEventListener('click', function(){
+  makeScreenVisible('login-screen');
+})
+
+
+document.getElementById('signup-button').addEventListener('click', function(){
+  makeScreenVisible('signup-screen');
+})
+
+document.getElementById('submit-signup-button').addEventListener('click', function(){
+  //confirm matching passwords
+  const email = document.getElementById("signup-email").value.trim();
+  const display_name = document.getElementById("signup-display").value.trim();
+  const password = document.getElementById("signup-password").value.trim();
+  const confirm = document.getElementById("signup-confirm").value.trim();
+  const language = document.getElementById("signup-language").value;
+  
+  const valid = confirmSignup(email, display_name, password, confirm);
+  
+  console.log(valid)
+  if(valid){
+    //send to server
+    socket.emit('new-user', {
+      email: email,
+      data: {
+        email: email,
+        name: display_name,
+        password: password,
+        friends: [],
+        interactions: [],
+        //language: language,
+        status: 0
+      }
+    })
+    makeScreenVisible("main-screen")
+  }
+})
+
+
+document.getElementById("home-button").addEventListener("click",() => {
+  makePageVisible("home-page");
+})
+
+
+document.getElementById("profile-button").addEventListener("click",() => {
+  makePageVisible("profile-page");
+})
+
+document.getElementById("friends-button").addEventListener("click",() => {
+  makePageVisible("friends-page");
+})
+
+document.getElementById("interactions-button").addEventListener("click",() => {
+  makePageVisible("interactions-page");
+})
+
+document.getElementById("big-red-button").addEventListener("click",() => {
+  
 })
 
 
 
-//add a listener to the socket for the header 'cpp output'
-socket.on('cpp output', (data) => {
-  console.log(`${data.filename}: ${data.output}`)
-})
+
+
+
+
+
+
 
